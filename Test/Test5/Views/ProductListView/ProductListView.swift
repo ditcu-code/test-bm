5dfb76a2919d41d746f20ee6535a7d2027e7f9ec
ditcu-code <work.adityac@gmail.com>
2023-06-25 00:42:35 +0700

    get request list of product

--- Test/Test5/Views/ProductListView/ProductListView.swift
+++ Test/Test5/Views/ProductListView/ProductListView.swift
@@ -6,32 +6,63 @@
 //
 
 import SwiftUI
+import Alamofire
 
 // TODO: List Product Screen (Test5b)
 
+struct ProductResponse: Decodable {
+    var responseCode: Int
+    var responseDesc: String
+    var data: [Product]
+    
+    struct Product: Decodable, Identifiable {
+        var productId: Int
+        var productName: String
+        var productImage: String
+        var productDesc: String
+        var productPrice: Int
+        
+        var id: Int {
+            return productId
+        }
+    }
+}
+
 struct ProductListView: View {
+    @StateObject var vm = ProductListViewModel()
+    
     var body: some View {
         NavigationView {
-            ScrollView {
-                ForEach(0..<5, id: \.self) {_ in
-                    HStack {
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text("Paket Kenyang 1").font(.headline)
-                            Text("Nasi Goreng Spesial + Es Teh").font(.caption)
-                            Text("28.000").font(.caption).bold()
-                        }
-                        
-                        Spacer()
-                        
-                        Image("nasi-goreng")
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 80, height: 80)
-                            .clipShape(RoundedRectangle(cornerRadius: 15))
-                    }.padding()
-                    Divider().padding(.horizontal)
+            VStack {
+                if vm.loading {
+                    CircularLoading()
+                } else {
+                    List(vm.products) { item in
+                        HStack {
+                            VStack(alignment: .leading, spacing: 12) {
+                                Text(item.productName).font(.headline)
+                                Text(item.productDesc).font(.caption)
+                                Text(item.productPrice.formatIDR()).font(.caption).bold()
+                            }
+                            
+                            Spacer()
+                            
+                            URLImage(urlString: item.productImage, imageSize: 80)
+                                .onAppear {
+                                    vm.loadImage(for: item)
+                                }
+                        }.padding(.vertical, 10)
+                    }
+                    .listStyle(.plain)
                 }
-            }.navigationTitle("Products")
+            }
+            .task {
+                await vm.fetchProducts()
+            }
+            .sheet(item: $vm.itemOpened) { item in
+                ProductDetailView()
+            }
+            .navigationTitle("Products")
         }
     }
 }
@@ -39,5 +70,87 @@ struct ProductListView: View {
 struct ProductListView_Previews: PreviewProvider {
     static var previews: some View {
         ProductListView()
+    }
+}
+
+class ProductListViewModel: ObservableObject {
+    @Published var products: [ProductResponse.Product] = []
+    @Published var itemOpened: ProductResponse.Product?
+    @Published var loadedImages: [Int: UIImage] = [:]
+    
+    @Published var loading: Bool = false
+    
+    private let decoder: JSONDecoder = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        return decoder
+    }()
+    
+    @MainActor
+    func fetchProducts() async {
+        self.loading = true
+
+        AF.request("https://aci-test.bmsecure.id/test/product").responseDecodable(of: ProductResponse.self, decoder: decoder) { response in
+            switch response.result {
+            case .success(let productResponse):
+                self.products = productResponse.data
+                self.loading = false
+            case .failure(let error):
+                print("Error fetching products: \(error)")
+                self.loading = false
+            }
+        }
+    }
+    
+    func loadImage(for product: ProductResponse.Product) {
+        guard loadedImages[product.productId] == nil else {
+            return // Image already loaded
+        }
+        
+        guard let url = URL(string: product.productImage) else {
+            return // Invalid image URL
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, let image = UIImage(data: data) else {
+                return // Error loading image
+            }
+            
+            DispatchQueue.main.async {
+                self.loadedImages[product.productId] = image
+            }
+        }.resume()
+    }
+
+}
+
+extension Int {
+    func formatIDR() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale(identifier: "id_ID") // Set locale to Indonesian
+
+        return numberFormatter.string(from: NSNumber(value: self)) ?? "-"
+    }
+}
+
+struct URLImage: View {
+    let urlString: String
+    let imageSize: CGFloat
+    
+    var body: some View {
+        AsyncImage(url: URL(string: urlString)) { image in
+            image
+                .resizable()
+                .scaledToFill()
+                .frame(width: imageSize, height: imageSize)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+        } placeholder: {
+            Image("placeholder-food")
+                .resizable()
+                .scaledToFill()
+                .frame(width: imageSize, height: imageSize)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+        }
     }
 }
