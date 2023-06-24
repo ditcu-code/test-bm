3fcaeacad3d10fb3cdb7abab85b359554f71163c
ditcu-code <work.adityac@gmail.com>
2023-06-24 15:42:21 +0700

    add test2 file

---
+++ Test/Test2/Test2.swift
@@ -0,0 +1,20 @@
+//
+//  Test2.swift
+//  Test
+//
+//  Created by Aditya Cahyo on 24/06/23.
+//
+
+import SwiftUI
+
+struct Test2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Test2_Previews: PreviewProvider {
+    static var previews: some View {
+        Test2()
+    }
+}
