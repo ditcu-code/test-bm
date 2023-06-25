dd0e8f3a0411e08371723730a74ff35410283c68
ditcu-code <work.adityac@gmail.com>
2023-06-25 14:01:45 +0700

    add height and width as parameter in urlimage

--- Test/Test5/Views/_Shared/URLImage.swift
+++ Test/Test5/Views/_Shared/URLImage.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 struct URLImage: View {
     let urlString: String
-    let imageSize: CGFloat
+
+    let width: CGFloat?
+    let height: CGFloat?
     
     var body: some View {
         AsyncImage(url: URL(string: urlString)) { image in
             image
                 .resizable()
                 .scaledToFill()
-                .frame(width: imageSize, height: imageSize)
+                .frame(width: width, height: height)
                 .clipShape(RoundedRectangle(cornerRadius: 15))
         } placeholder: {
             Image("placeholder-food")
                 .resizable()
                 .scaledToFill()
-                .frame(width: imageSize, height: imageSize)
+                .frame(width: width, height: height)
                 .clipShape(RoundedRectangle(cornerRadius: 15))
         }
     }
@@ -30,7 +32,7 @@ struct URLImage: View {
 
 struct URLImage_Previews: PreviewProvider {
     static var previews: some View {
-        URLImage(urlString: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Male_and_female_chicken_sitting_together.jpg/180px-Male_and_female_chicken_sitting_together.jpg", imageSize: 150)
+        URLImage(urlString: "https://adityacahyo.com/og.png", width: 150, height: 150)
     }
 }
 
