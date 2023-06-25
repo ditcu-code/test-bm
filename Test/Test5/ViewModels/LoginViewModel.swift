27d37d835fe0073019601968e9c9bc0233d24902
ditcu-code <work.adityac@gmail.com>
2023-06-25 15:42:45 +0700

    add login functionality and its error

--- Test/Test5/ViewModels/LoginViewModel.swift
+++ Test/Test5/ViewModels/LoginViewModel.swift
@@ -8,11 +8,33 @@
 import Foundation
 
 class LoginViewModel: ObservableObject {
-    @Published var username: String = ""
-    @Published var password: String = ""
-    @Published var via: String = ""
+    @Published var username: String = "aci"
+    @Published var password: String = "akucintaindonesia"
+    @Published var via: String = "ios"
     
+    @Published var name: String?
     @Published var loading: Bool = false
     @Published var errorMessage: String = ""
     
+    private let apiManager = APIManager.shared
+    
+    func login() {
+        let body: [String : Any] = ["username" : username, "password" : password, "via" : via]
+        
+        apiManager.login(parameters: body) { [weak self] user, error, errorDesc in
+            guard let self = self else { return }
+            
+            DispatchQueue.main.async {
+                self.loading = false
+                
+                if let errorDesc = errorDesc {
+                    self.errorMessage = errorDesc
+                } else if let user = user {
+                    UserDefaults.standard.set(true, forKey: "isLoggedIn")
+                    self.name = user.data.name
+                    self.errorMessage = ""
+                }
+            }
+        }
+    }
 }
