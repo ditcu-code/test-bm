5e0ab37c2d2c379f0a67d4e0938d6cb57584633c
ditcu-code <work.adityac@gmail.com>
2023-06-25 14:01:20 +0700

    refactor the productmodel

--- Test/Test5/Models/ProductModel.swift
+++ Test/Test5/Models/ProductModel.swift
@@ -7,20 +7,26 @@
 
 import Foundation
 
-struct ProductResponse: Decodable {
+struct ProductsResponse: Decodable {
     var responseCode: Int
     var responseDesc: String
     var data: [Product]
+}
+
+struct ProductDetailResponse: Decodable {
+    var responseCode: Int
+    var responseDesc: String
+    var data: Product
+}
+
+struct Product: Decodable, Identifiable {
+    var productId: Int
+    var productName: String
+    var productImage: String
+    var productDesc: String
+    var productPrice: Int
     
-    struct Product: Decodable, Identifiable {
-        var productId: Int
-        var productName: String
-        var productImage: String
-        var productDesc: String
-        var productPrice: Int
-        
-        var id: Int {
-            return productId
-        }
+    var id: Int {
+        return productId
     }
 }
